87403e0700ee9340cab9e563628893b80effbb90
Brendan McDonald <brendan@brendandmcdonald.com>
2013-01-17 23:40:25 -0800

    tested the features on my local server and it's working

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './master'
+run Sinatra::Application
--- master.rb
+++ master.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  File.read(File.join('public', 'index.html'))
+	File.read(File.join('public', 'index.html'))
 end
 get '/topiary' do
 	"that's a kind of tree, right?"
