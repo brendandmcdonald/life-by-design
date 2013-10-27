556aa9df905a0fed70c982caa2ea8f804ce08649
Brendan McDonald <brendan@brendandmcdonald.com>
2013-10-27 15:24:27 -0700

    simplified site and worked on quote formatting

--- master.rb
+++ master.rb
@@ -7,41 +7,11 @@ get '/' do
 	File.read(File.join('public', 'home.html'))
 end
 
+=begin
 get '/quotes' do
 	File.read(File.join('public', 'quotes.html'))
 end
-
-get '/give_thanks' do
-	File.read(File.join('public', 'give_thanks.html'))
-end
-
-get '/trello' do
-	File.read(File.join('public', 'trello.html'))
-end
-
-get '/blog' do
-	File.read(File.join('public', 'blog.html'))
-end
-
-get '/contacts' do
-	File.read(File.join('public', 'contacts.html'))
-end
-
-get '/journal' do
-	File.read(File.join('public', 'journal.html'))
-end
-
-get '/codecademy' do
-	File.read(File.join('public', 'codecademy.html'))
-end
-
-get '/jquery' do
-	File.read(File.join('public', 'jquery.html'))
-end
-
-get '/quotes' do
-	File.read(File.join('public', 'quotes.html'))
-end
+=end
 
 =begin
 get '/topiary' do
