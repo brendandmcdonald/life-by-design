a91c50fa9e40edc5d7f90eff302fe7581d20890c
Brendan McDonald <brendan@brendandmcdonald.com>
2015-04-22 10:32:14 +0100

    working on background

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var bgArray = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg'];
     var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
 
-    $('body').css('background', bg);
+    //$('body').css('background', bg);
 
     // If you have defined a path for the images
     var path = 'images/';
