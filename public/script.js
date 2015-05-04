ef79bed9a3572b232248928a3ef74a4adb584814
Brendan McDonald <brendan@brendandmcdonald.com>
2015-05-04 20:59:14 +0100

    updated quotes

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-
-
 function displayAdvice(){
     var adviceToDisplay = getAdvice();
     document.getElementById('advice').innerHTML = adviceToDisplay;
@@ -67,6 +65,7 @@ var advice = [
     ["Compound effort moves mountains.",""],
     ["If you are not failing, you are not trying hard enough.",""],
     ["Seize the moment.",""],
+    ["The future will be built by opinionated technicians, not by theorists and generalists looking for a problem to solve.",""],
     ["zest: have great enthusiasm and energy","grit: keep resolve when faced with unpleasant duty or task","self-control: mastering one’s impulses","social intelligence: get along well with others, and have them coordinate with you","gratitude: express thanks to those who have benefited you","optimism: expect good things","curiosity: explore the unknown with relish."],
     ["Stick to your plan. It works.",""],
     ["Never let your persistence and passion turn into stubbornness and ignorance.","Anthony J. D'Angelo"],
@@ -77,6 +76,7 @@ var advice = [
     ["The difference between a successful person and others is not a lack of strength, not a lack of knowledge, but rather a lack of determination.","Vince Lombardi"],
     ["Adhere to your purpose and you will soon feel as well as you ever did. On the contrary, if you falter, and give up, you will lose the power of keeping any resolution, and will regret it all your life.","Abraham Lincoln"],
     ["Test fast, fail fast, adjust fast.","Tom Peters"],
+    ["I don’t think change can be planned. I think it’s only something that can be recognized, after the fact.","Jad Abumrad, Founder of RadioLab"],
     ["Are you placing enough interesting, freakish, long shot, weirdo bets?","Tom Peters"],
     ["Only those who risk going too far can possibly find out how far they can go.","T.S. Eliot"],
     ["The secret to my success is that I bit off more than I could chew and chewed as fast as I could.","Paul Hogan"],
@@ -84,14 +84,16 @@ var advice = [
     ["People will rise to meet seemingly insurmountable obstacles and challenges if they understand the worthiness of the personal sacrifices and effort. Supporting that understanding must be mentors who provide leadership; without both ingredients, a cause will go unrealized and a mission is likely to fail.","Glenn R. Jones"],
     ["Focus. If you chase two rabbits, both will escape.",""],
     ["Here is the prime condition of success: Concentrate your energy, thought and capital exclusively upon the business in which you are engaged. Having begun on one line, resolve to fight it out on that line, to lead in it, adopt every improvement, have the best machinery, and know the most about it.","Andrew Carnegie"],
-    ["You are the average of the 5 people you spend the most time with.",""],
+    ["You are the average of the 5 people with whom you spend the most time.",""],
     ["On choices: what is most likely to change your life in a good way?","~"],
     ["DEGAF. Break social norms often, cultivate discomfort, stretch yourself. What you fear to do is what you must do.",""],
     ["If today was the last day of your life, how would you be spending it?",""],
     ["Smile. Rigt now smile. Seriously. Isn't that better?",""],
     ["We consistently get the order wrong when attriuting cause and effect. To learn, first teach. To change the way you think, first change the way you act. The stories we tell ourselves about cause and effect are often postrationalizations to ascribe rationality to a subconscious process. Faking a smile makes you happier. In other words, don't try to think your way into a new way of acting, just act your way into a new way of thinking.",""],
     ["You've come a long way, baby.",""],
+    ["MAXIMIZE YOUR 20s: 1. Don’t have an identity crisis: get identity capital. Do something that supports where you might want to go. 2. Use your weak ties. Get outside your inner circle—get to your weak ties. 3. Choose who and what you want.","TED Talk on your 20s"],
     ["Be bold, be brief, be gone.",""],
+    ["USABILITY", "Learnability: How easy is it for users to accomplish basic tasks the first time they encounter the interface?", "Efficiency: How quickly can users accomplish their tasks after they learn how to use the interface?", "Memorability: After a period of non-use, how long does it take users to reestablish proficiency?", "Errors: How many errors do users make, how severe are these errors, and how easy is it for users to recover from these errors?", "Satisfaction: How pleasant or satisfying is it to use the interface?",""],
     ["Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.","Rumi"],
     ["Out beyond ideas of wrongdoing and rightdoing there is a field. I'll meet you there. When the soul lies down in that grass the world is too full to talk about","Rumi"],
     ["Don't ask yourself what the world needs. Ask yourself what makes you come alive and then go do that. Because what the world needs is people who have come alive.","Dr. Howard Thurman"],
@@ -100,6 +102,8 @@ var advice = [
     ["I will go anywhere as long as it is forward.","David Livingston"],
     ["The best way out is always through.","Robert Frost"],
     ["Every strike brings me closer to the next home run.","Babe Ruth"],
+    ["Are you doing something that will advance your goals?","What's the most fun way you could advance your goals right now?"],
+    ["What's the best use of your next 3 minutes? Do it, now."],
     ["Don’t wait. The time will never be just right.","Napoleon Hill"],
     ["Everything you’ve ever wanted is on the other side of fear.","George Addair"],
     ["It is never too late to be what you might have been.","George Eliot"],
@@ -122,8 +126,7 @@ var advice = [
     ["My best friend is the one who brings out the best in me.","Henry Ford"],
     ["Tough times never last, but tough people do.","Dr. Robert Schuller"],
     ["There is only one success: to be able to spend your life in your own way.","Christopher Morley"],
-    ["The best time to plant a tree is twenty years ago. The second best time is now.","Chinese Proverb"]
-    ["The power of imagination makes us infinite.","John Muir"],
+    ["The best time to plant a tree is twenty years ago. The second best time is now.","Chinese Proverb"],
     ["The best dreams happen when you’re awake.","Cherie Gilderbloom"],
     ["Once you choose hope, anything’s possible.","Christopher Reeve"],
     ["Believe and act as if it were impossible to fail.","Charles Kettering"],
